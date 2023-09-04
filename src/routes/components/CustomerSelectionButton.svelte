2a4cc83ce60377bdf3b6e76b3df2db0a5afa9579
mtsdlf <mts@gmail.com>
2023-09-03 21:46:31 -0300

    inital commit

---
+++ src/routes/components/CustomerSelectionButton.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  function selectCustomer() {
+    // Implement your customer selection logic here, and obtain the selected customer
+    const selectedCustomer = {
+      id: 1, // Replace with the actual customer ID
+      name: 'Customer Name', // Replace with the actual customer name
+      email: 'customer@email.com', // Replace with the actual customer email
+    };
+
+    // Dispatch an event to notify the parent component (App.svelte) with the selected customer
+    dispatch('select', { customer: selectedCustomer });
+  }
+</script>
+
+<button on:click={selectCustomer}>Select Customer</button>
+<style>
+  
+</style>
